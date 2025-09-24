cc543f6c12f97f844ea471ce7e09964ba13634c5
luvkapoipoi <deltaidk21@gmail.com>
2025-09-24 11:38:54 +0400

    Update print.py

--- print.py
+++ print.py
@@ -1,1 +1,9 @@
-print ("Hello, my name is Achilles Maling. I am 20 years old and I study in BathSpa University RAK.")
+""" We shall learn basics of python programming
+this section is about comments """
+print ("Hello, my name is Achilles Maling. I am 20 years old and I study in BathSpa University RAK.") #single line comment
+
+print ("Welcome to python programming")
+
+age = 20 #assigning value
+
+print (age)
